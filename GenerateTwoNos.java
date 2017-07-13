546882265544bb37ddf6764ed06e09230bdea7d4
Ankit Arni <ankit.arni.1995@gmail.com>
2017-07-13 15:35:03 +0530

    Added GenerateTwoNos.java

---
+++ GenerateTwoNos.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+class GenerateTwoNos
+{
+	public int randomNumber()
+	{
+		int randNum = rand.nextInt(11);
+		return randNum;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Generating 2 random numbers between the range (0-10):-");
+		int num1 = randomNumber();
+		int num2 = randomNumber();
+		System.out.println("Numbers generated are " + a + " & " + b);
+	}
+}
